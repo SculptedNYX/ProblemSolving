cedb45b2870660797d3726527e12d34a6d331ae7
SculptedNyx <DarkValvet@protonmail.com>
2023-02-27 23:27:31 +0200

    solved the problem using O(n^2)

--- TwoSum/twosum.c
+++ TwoSum/twosum.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
 
 int main()
 {
-    printf("bonany");
+    int num[] = {0,4,3,0};
+    int* arr = twoSum(num, 4, 0, NULL);
+    if(arr != NULL)
+    {
+        printf("[%d,%d]", arr[0], arr[1]);
+    }
+    else
+    {
+        printf("\nthe array doesnt have any elements");
+    }
+
     return 0;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    //*returnSize = 2; // this part is needed for leetcode
+    int* returnArr = (int*) malloc(sizeof(int)*2);
+    for (int i = 0; i < numsSize; i++)
+    { 
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                returnArr[0] = i;
+                returnArr[1] = j;
+                return returnArr;
+            }
+        }
+        
+    }
+    return NULL;
 }
